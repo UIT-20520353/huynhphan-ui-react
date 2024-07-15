eb675a501df9df84a13cf45b63407f89ab163c20
vuongtx21 <84366191+UIT-20520353@users.noreply.github.com>
2024-07-15 07:51:21 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import HomePage from "@/features/home";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/features/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <span className="text-3xl font-bold">Home page</span>
+    </div>
+  );
+};
+
+export default HomePage;
